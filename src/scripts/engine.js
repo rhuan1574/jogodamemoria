84a492f750033ba0e3729d422f0be870c5107603
PedroMarques162 <pedropaulomarquesda@gmail.com>
2025-05-22 13:29:05 -0300

    mud

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -27,3 +27,32 @@ for (let i = 0; i < emojis.length; i++) {
   box.onclick = handleClick;
   document.querySelector(".game").appendChild(box);
 }
+
+function handleClick() {
+  if (openCards.length < 2) {
+    this.classList.add("boxOpen");
+    openCards.push(this);
+  }
+
+  if (openCards.length == 2) {
+    setTimeout(checkMatch, 500);
+  }
+
+  console.log(openCards);
+}
+
+function checkMatch() {
+  if (openCards[0].innerHTML === openCards[1].innerHTML) {
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+  } else {
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+  }
+
+  openCards = [];
+
+  if (document.querySelectorAll(".boxMatch").length === emojis.length) {
+    alert("Você venceu !");
+  }
+}
